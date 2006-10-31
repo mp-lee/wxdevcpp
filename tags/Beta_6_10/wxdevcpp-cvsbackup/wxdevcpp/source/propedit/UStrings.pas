329e499cbf729ac72d8ac0e3eb9288c37d3e484c
nobody <nobody@917957a4-63a1-4874-a357-9ccf276ff571>
2006-10-31 08:03:52 +0000

    This commit was manufactured by cvs2svn to create tag 'Beta_6_10'.

---
+++ tags/Beta_6_10/wxdevcpp-cvsbackup/wxdevcpp/source/propedit/UStrings.pas
@@ -0,0 +1,59 @@
+unit UStrings;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, Buttons, ExtCtrls, XPMenu;
+
+type
+  TStringsForm = class(TForm)
+    btnOK: TBitBtn;
+    btnCancel: TBitBtn;
+    btnHelp: TBitBtn;
+    grpMemo: TGroupBox;
+    Memo: TMemo;
+    XPMenu: TXPMenu;
+
+    procedure MemoChange(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    OnContentsChanged: TNotifyEvent;
+  end;
+
+var
+  StringsForm: TStringsForm;
+
+implementation
+uses
+  devCfg;
+
+{$R *.DFM}
+
+procedure TStringsForm.MemoChange(Sender: TObject);
+var
+  lbl: string;
+begin
+    if Memo.Lines.Count = 1 then
+        lbl := ' Line'
+    else
+        lbl := ' Lines';
+    
+    grpMemo.Caption := IntToStr(Memo.Lines.Count) + lbl;
+
+    //Broadcast the event
+    if Assigned(OnContentsChanged) then
+        OnContentsChanged(Sender);
+end;
+
+procedure TStringsForm.FormShow(Sender: TObject);
+begin
+    Memo.SetFocus;
+    if devData.XPTheme then
+        XPMenu.Active := true;
+end;
+
+end.
