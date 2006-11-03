877858e10b900f3c2a424d36fc83f8a885318f9a
lowjoel <lowjoel@917957a4-63a1-4874-a357-9ccf276ff571>
2006-11-03 11:03:20 +0000

    Fixed DLL makefile generation

--- branches/VC_Debugger/wxdevcpp-cvsbackup/wxdevcpp/source/devcfg.pas
+++ branches/VC_Debugger/wxdevcpp-cvsbackup/wxdevcpp/source/devcfg.pas
@@ -2464,7 +2464,7 @@ begin
     fLinkerFormat           := '-o "%s"';
     fLinkerPaths            := '-L"%s"';
     fIncludeFormat          := '-I"%s"';
-    fDllFormat              := '--out-implib "lib%s" -o %s';
+    fDllFormat              := '--implib "%s" -o %s';
     fLibFormat              := 'rcu "%s"';
     fPchCreateFormat        := '';
     fPchUseFormat           := '';
